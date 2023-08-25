1903641ce1d9095583c818cca1e12fdf07aba182
Erick-Mercedes <ing.erick.mercedes12@gmail.com>
2023-08-24 20:49:23 -0400

    fist

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Pruebas de Flutter',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
@@ -54,7 +54,7 @@ class _ContadorState extends State<Contador> {
           start: 142,
           child:
               // Demo
-               Text("Demo",
+              Text("Demo",
                   style: TextStyle(
                       color: Color(0xffffffff),
                       fontWeight: FontWeight.w400,
